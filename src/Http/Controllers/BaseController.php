88408999cbe2e9da35c8aa129a464cb9ed138f9a
David Bonner <david@continuuminteractive.com>
2017-06-13 13:22:11 +0100

    updated basecontroller

--- src/Http/Controllers/BaseController.php
+++ src/Http/Controllers/BaseController.php
@@ -3,11 +3,12 @@
 namespace Continuum\Support\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class BaseController
+class BaseController extends Controller
 {
     use AuthorizesRequests, ValidatesRequests;
 
